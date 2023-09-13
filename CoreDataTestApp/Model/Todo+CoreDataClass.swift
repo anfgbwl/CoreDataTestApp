18035ad7b2f4a27287458c10e19069b9983ccba2
Seoon KIM <6872065@gmail.com>
2023-09-13 14:08:43 +0900

    feat: Core Data 및 TableView 생성

---
+++ CoreDataTestApp/Model/Todo+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Todo+CoreDataClass.swift
+//  CoreDataTestApp
+//
+//  Created by t2023-m0076 on 2023/09/13.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Todo)
+public class Todo: NSManagedObject {
+
+}
--- CoreDataTestApp/ViewController.swift
+++ CoreDataTestApp/ViewController.swift
@@ -8,12 +8,61 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Variables
+    private var todo: [Todo]?
+    
+    // MARK: - UI Conponents
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .systemBackground
+        tableView.separatorColor = .gray
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
+    private let addButton: UIBarButtonItem = {
+        let addButton = UIBarButtonItem()
+        addButton.image = UIImage(systemName: "plus")
+        return addButton
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupUI()
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    private func setupUI() {
+        self.navigationItem.rightBarButtonItem = self.addButton
+        
+        self.view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+        ])
+        
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todo?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let todo = todo![indexPath.row]
+        cell.textLabel?.text = "todo"
+        return cell
+    }
+    
+}
