433302ef471f63cde741413be84e6fd35432704c
Seoon KIM <6872065@gmail.com>
2023-09-13 15:02:14 +0900

    feat: Add Todo Alert 생성

--- CoreDataTestApp/ViewController.swift
+++ CoreDataTestApp/ViewController.swift
@@ -11,7 +11,8 @@ import SnapKit
 class ViewController: UIViewController {
     
     // MARK: - Variables
-    private var todo: [Todo]?
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    private var todos: [Todo]?
     
     // MARK: - UI Conponents
     private let tableView: UITableView = {
@@ -28,14 +29,57 @@ class ViewController: UIViewController {
         return addButton
     }()
 
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupUI()
         tableView.dataSource = self
         tableView.delegate = self
+        addButton.target = self
+        addButton.action = #selector(addTapped)
+        
+        fetchTodo()
     }
     
+    // MARK: - function
+    private func fetchTodo() {
+        do {
+            self.todos = try context.fetch(Todo.fetchRequest())
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        } catch {
+            print("fetchTodo Error")
+        }
+        
+    }
+    
+    @objc private func addTapped() {
+        let alert = UIAlertController(title: "Add Todo", message: "", preferredStyle: .alert)
+        alert.addTextField{ (textField) in
+            textField.placeholder = "Please write your todo."
+        }
+        let add = UIAlertAction(title: "Add", style: .default) { _ in
+            let textField = alert.textFields![0]
+            let newTodo = Todo(context: self.context)
+            newTodo.todo = textField.text
+            newTodo.createDate = Date()
+            
+            do {
+                try self.context.save()
+            } catch {
+                print("save error")
+            }
+            
+            self.fetchTodo()
+        }
+        
+        alert.addAction(add)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: - setupUI
     private func setupUI() {
         self.navigationItem.rightBarButtonItem = self.addButton
         
@@ -50,15 +94,16 @@ class ViewController: UIViewController {
 
 }
 
+// MARK: - NORMAL CELL
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return todo?.count ?? 0
+        return todos?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let todo = todo![indexPath.row]
-        cell.textLabel?.text = "todo"
+        let todo = todos![indexPath.row]
+        cell.textLabel?.text = todo.todo
         return cell
     }
     
